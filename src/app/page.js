7ce586af687f7ff8a5f8f824b176f2b496c79d40
aitizaz <atz.softprgmr@gmail.com>
2024-11-29 18:13:27 +0500

    added changes

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,13 @@ import testdata from "./data/testdata/testherodata.json";
 export default function Home() {
   return (
     <>
-      <MainHeroSection
-        title={testdata.title}
-        paragraph={testdata.paragraph}
-        forwardImage={testdata.forwardImage}
-      />
+      <main>
+        <MainHeroSection
+          title={testdata.title}
+          paragraph={testdata.paragraph}
+          forwardImage={testdata.forwardImage}
+        />
+      </main>
     </>
   );
 }
