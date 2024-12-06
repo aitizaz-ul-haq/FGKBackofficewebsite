28667af2982dc217186eb48b816b1e64e79df4fe
aitizaz <atz.softprgmr@gmail.com>
2024-12-06 19:11:47 +0500

    added changes

--- src/app/components/pagecomponents/careerspagecomps/careersopeningssection.js
+++ src/app/components/pagecomponents/careerspagecomps/careersopeningssection.js
@@ -1,24 +1,21 @@
+import React from "react";
 import "./styles/careersopeningssection.css";
+import openingsdata from "./data/careersopeningsdata.json";
 
 export default function CareersOpeningsSection() {
   return (
-    <>
-      <div className="openings-container">
-        <h2 className="openings-heading font-roboto-sarif text-light">
-          Openings
-        </h2>
-        <div className="position-strip">
+    <div className="openings-container">
+      <h2 className="openings-heading font-roboto-sarif text-light">
+        Openings
+      </h2>
+      {openingsdata.map((opening, index) => (
+        <div key={index} className="position-strip">
           <div className="position-description-details-button">
             <h3 className="position-heading font-roboto-sarif">
-              Position Designation
+              {opening.designation}
             </h3>
             <p className="position-description font-inter">
-              simply dummy text of the printing and typesetting industry. Lorem
-              Ipsum has been the industry's standard dummy text ever since the
-              1500s, when an unknown printer took a galley simply dummy text of
-              the printing and typesetting industry. Lorem Ipsum has been the
-              industry's standard dummy text ever since the 1500s, when an
-              unknown printer took a galley
+              {opening.description}
             </p>
           </div>
           <div className="careers-details-button-container">
@@ -27,49 +24,7 @@ export default function CareersOpeningsSection() {
             </button>
           </div>
         </div>
-
-        <div className="position-strip">
-          <div className="position-description-details-button">
-            <h3 className="position-heading font-roboto-sarif">
-              Position Designation
-            </h3>
-            <p className="position-description font-inter">
-              simply dummy text of the printing and typesetting industry. Lorem
-              Ipsum has been the industry's standard dummy text ever since the
-              1500s, when an unknown printer took a galley simply dummy text of
-              the printing and typesetting industry. Lorem Ipsum has been the
-              industry's standard dummy text ever since the 1500s, when an
-              unknown printer took a galley
-            </p>
-          </div>
-          <div className="careers-details-button-container">
-            <button className="position-strip-button font-inter">
-              Details
-            </button>
-          </div>
-        </div>
-
-        <div className="position-strip">
-          <div className="position-description-details-button">
-            <h3 className="position-heading font-roboto-sarif">
-              Position Designation
-            </h3>
-            <p className="position-description font-inter">
-              simply dummy text of the printing and typesetting industry. Lorem
-              Ipsum has been the industry's standard dummy text ever since the
-              1500s, when an unknown printer took a galley simply dummy text of
-              the printing and typesetting industry. Lorem Ipsum has been the
-              industry's standard dummy text ever since the 1500s, when an
-              unknown printer took a galley
-            </p>
-          </div>
-          <div className="careers-details-button-container">
-            <button className="position-strip-button font-inter">
-              Details
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
+      ))}
+    </div>
   );
 }
