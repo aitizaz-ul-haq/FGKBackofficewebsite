2ad185dab1744149924b752934a4548472edabe6
aitizaz <atz.softprgmr@gmail.com>
2024-12-10 16:02:39 +0500

    added changes

--- src/app/components/layoutcomponents/cards/roundedendscard/roundendscard.js
+++ src/app/components/layoutcomponents/cards/roundedendscard/roundendscard.js
@@ -1,38 +1,78 @@
 import "./roundendscards.css";
 import Image from "next/image";
+import cardsData from "../../../../components/pagecomponents/servicespagecomps/data/roundcarddata.json";
 
 export default function RoundEndsCards() {
   return (
-    <>
-      <div className="roundendscard-container">
-        <div className="top-card">
-          <div className="left-circular-section">
+    <div className="roundendscard-container">
+      {cardsData.map((card, index) => (
+        <div
+          key={card.id}
+          className={index % 2 === 0 ? "bottom-card" : "top-card"}
+        >
+          <div className="top-card-mobile-image">
             <Image
-              src="/operations_support_circular_icon.png"
-              alt="card icon"
-              width={400}
-              height={400}
+              src={card.iconMobile}
+              alt={`${card.title} icon`}
+              width={100}
+              height={100}
               className="teampageteamcard-mobile-image"
             />
           </div>
-          <div className="right-details-section">
-            <div className="right-roundedcard-heading">Operations Support</div>
-            <div className="right-roundedcard-description">
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using 'Content here,
-              content here', making it look like readable English. Many desktop
-              publishing packages and web page editors now use Lorem Ipsum as
-              their default model text, and a
-            </div>
-            <div className="right-roundedcard-arrowhead-button">
-              Consult with Us
-            </div>
-          </div>
+
+          {index % 2 === 0 ? (
+            <>
+              {/* Right Details Section */}
+              <div className="right-details-section rounded-cards-details-margin">
+                <div className="right-roundedcard-heading font-inter text-light">
+                  {card.title}
+                </div>
+                <div className="right-roundedcard-description font-inter text-light">
+                  {card.description}
+                </div>
+                <div className="right-roundedcard-arrowhead-button font-inter text-light">
+                  {card.buttonText}
+                </div>
+              </div>
+              {/* Left Circular Section */}
+              <div className="left-circular-section">
+                <Image
+                  src={card.iconCircular}
+                  alt={`${card.title} circular icon`}
+                  width={410}
+                  height={410}
+                  className="teampageteamcard-main-image"
+                />
+              </div>
+            </>
+          ) : (
+            <>
+              {/* Left Circular Section */}
+              <div className="left-circular-section">
+                <Image
+                  src={card.iconCircular}
+                  alt={`${card.title} circular icon`}
+                  width={410}
+                  height={410}
+                  className="teampageteamcard-main-image"
+                />
+              </div>
+              {/* Right Details Section */}
+              <div className="right-details-section rounded-cards-details-margin">
+                <div className="right-roundedcard-heading font-inter text-light">
+                  {card.title}
+                </div>
+                <div className="right-roundedcard-description font-inter text-light">
+                  {card.description}
+                </div>
+                <div className="right-roundedcard-arrowhead-button font-inter text-light">
+                  {card.buttonText}
+                </div>
+              </div>
+            </>
+          )}
         </div>
-        <div className="bottom-card"></div>
-      </div>
-    </>
+      ))}
+    </div>
   );
 }
