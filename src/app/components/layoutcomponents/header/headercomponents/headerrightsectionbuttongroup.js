87ff76e9e0354b2c49c4195613612e5e6f6ec8ec
aitizaz <atz.softprgmr@gmail.com>
2024-12-17 17:18:56 +0500

    added changes

--- src/app/components/layoutcomponents/header/headercomponents/headerrightsectionbuttongroup.js
+++ src/app/components/layoutcomponents/header/headercomponents/headerrightsectionbuttongroup.js
@@ -1,21 +1,44 @@
+"use client";
+
 import Link from "next/link";
 import headerButtons from "../headerdata/headerButtons.json";
 import HeaderContactButton from "@/app/components/uicomponents/buttons/headercontactbutton";
+import { useState } from "react";
 
 export default function HeaderRightSectionButtonSection() {
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
   return (
     <>
       {headerButtons.map((button, index) => (
-        <Link
-          style={{ textDecoration: "none" }}
+        <div
           key={index}
-          href={button.link}
-          passHref
+          className="header-button-wrapper"
+          onMouseEnter={() => setHoveredIndex(index)}
+          onMouseLeave={() => setHoveredIndex(null)}
         >
-          <div className="header-nobackground-button font-inter text-light">
-            {button.name}
-          </div>
-        </Link>
+          <Link style={{ textDecoration: "none" }} href={button.link} passHref>
+            <div className="header-nobackground-button font-inter text-light">
+              {button.name}
+            </div>
+          </Link>
+
+          {/* Subpages Dropdown */}
+          {button.subpages && hoveredIndex === index && (
+            <div className="header-dropdown-menu">
+              {button.subpages.map((subpage, subIndex) => (
+                <Link
+                  key={subIndex}
+                  href={subpage.link}
+                  passHref
+                  className="header-dropdown-item font-inter"
+                >
+                  {subpage.name}
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
       ))}
       <HeaderContactButton name="Contact" link="/contact" />
     </>
