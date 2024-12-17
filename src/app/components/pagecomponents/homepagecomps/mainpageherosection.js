7c22e32d92ae2f25bdf52c486cccacecf61b6f7e
aitizaz <atz.softprgmr@gmail.com>
2024-12-17 15:25:46 +0500

    added changes

--- src/app/components/pagecomponents/homepagecomps/mainpageherosection.js
+++ src/app/components/pagecomponents/homepagecomps/mainpageherosection.js
@@ -3,12 +3,7 @@ import herodata from "./data/herosection.json";
 
 export default function MainPageHeroSection({ title, paragraph }) {
   return (
-    <div
-      className="main-page-hero-section"
-      // style={{
-      //   backgroundImage: `url('/main-hero.png')`,
-      // }}
-    >
+    <div className="main-page-hero-section">
       <div className="main-page-hero-content">
         <h1 className="main-page-hero-title font-roboto-serif text-extralight">
           {herodata.title}
@@ -16,6 +11,9 @@ export default function MainPageHeroSection({ title, paragraph }) {
         <p className="main-page-hero-paragraph font-inter text-extralight">
           {herodata.paragraph}
         </p>
+        <div className="main-page-hero-button font-inter">
+          Get Expert Advice
+        </div>
       </div>
     </div>
   );
