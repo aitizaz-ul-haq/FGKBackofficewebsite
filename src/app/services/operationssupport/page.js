18abe96d0966355459d40741632aeda3011f435d
aitizaz <atz.softprgmr@gmail.com>
2025-01-03 18:13:20 +0500

    added changes

--- src/app/services/operationssupport/page.js
+++ src/app/services/operationssupport/page.js
@@ -1,6 +1,10 @@
 import SubPageHeroSection from "@/app/components/subpagecomponenets/subpageherosection/subpageherosection";
 import { getHeroData } from "@/app/utils/gethreodata";
 import OperatSupGridSection from "@/app/components/subpagecomponenets/operatsup/operatsupgridsection";
+import SecondSectionTypeMixed from "@/app/components/subpagecomponenets/secondsectiontypes/secondsectiontypemixed";
+import operatetextdata from "@/app/data/subpagedata/operatsup/secondsectionoperatsudata/operatsupsecondsectiontextdata.json";
+import operateleftstripdata from "@/app/data/subpagedata/operatsup/secondsectionoperatsudata/operatesupleftstripdata.json";
+import operaterightstripdata from "@/app/data/subpagedata/operatsup/secondsectionoperatsudata/operatesuprightstripdata.json";
 
 export default function OperationsSupport() {
   const data = getHeroData("operations-support");
@@ -8,6 +12,11 @@ export default function OperationsSupport() {
     <>
       <SubPageHeroSection data={data} />
       <OperatSupGridSection />
+      <SecondSectionTypeMixed
+        textdata={operatetextdata}
+        leftstripcarddata={operateleftstripdata}
+        rightstripcarddata={operaterightstripdata}
+      />
     </>
   );
 }
