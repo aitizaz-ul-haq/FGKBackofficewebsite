a5e5c003d4e15b28ab30c15850c68696fe10522a
aitizaz <atz.softprgmr@gmail.com>
2025-01-06 11:04:55 +0500

    added changes

--- src/app/services/humanresources/page.js
+++ src/app/services/humanresources/page.js
@@ -1,6 +1,9 @@
 import SubPageHeroSection from "@/app/components/subpagecomponenets/subpageherosection/subpageherosection";
 import { getHeroData } from "@/app/utils/gethreodata";
 import HumResGridSection from "@/app/components/subpagecomponenets/humres/humresgridsection";
+import SecondSectionRightSided from "@/app/components/subpagecomponenets/secondsectiontypes/secondsectiontyperightsided";
+import humrestextdata from "@/app/data/subpagedata/humresdata/secondsectionhumanresourcesdata/humrestextdata.json";
+import humresrightsidestripdata from "@/app/data/subpagedata/humresdata/secondsectionhumanresourcesdata/humresrightstripdata.json";
 
 export default function HumanResources() {
   const data = getHeroData("human-resources");
@@ -8,6 +11,10 @@ export default function HumanResources() {
     <>
       <SubPageHeroSection data={data} />
       <HumResGridSection />
+      <SecondSectionRightSided
+        textdata={humrestextdata}
+        rightstripcarddata={humresrightsidestripdata}
+      />
     </>
   );
 }
