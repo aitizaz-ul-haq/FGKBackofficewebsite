641c67917b9215294502fc6fcb240f818612eb31
aitizaz <atz.softprgmr@gmail.com>
2025-01-06 15:17:16 +0500

    added changes

--- src/app/services/accountingandreporting/page.js
+++ src/app/services/accountingandreporting/page.js
@@ -1,6 +1,10 @@
 import SubPageHeroSection from "@/app/components/subpagecomponenets/subpageherosection/subpageherosection";
 import { getHeroData } from "@/app/utils/gethreodata";
 import AccRepGridSection from "@/app/components/subpagecomponenets/accandrep/accrepgridsection";
+import SecondSectionTypeTopAndBottom from "@/app/components/subpagecomponenets/secondsectiontypes/secondsectiontypetopandbottom";
+import accreptextdata from "@/app/data/subpagedata/accandrepdata/secondsectionaccrep/accreptextdata.json";
+import accrepleftstripdata from "@/app/data/subpagedata/accandrepdata/secondsectionaccrep/accrepleftstripdata.json";
+import accreprightstripdata from "@/app/data/subpagedata/accandrepdata/secondsectionaccrep/accreprightstripdata.json";
 
 export default function AccountingAndReporting() {
   const data = getHeroData("accounting-&-reporting");
@@ -8,6 +12,11 @@ export default function AccountingAndReporting() {
     <>
       <SubPageHeroSection data={data} />
       <AccRepGridSection />
+      <SecondSectionTypeTopAndBottom
+        textdata={accreptextdata}
+        leftstripcarddata={accrepleftstripdata}
+        rightstripcarddata={accreprightstripdata}
+      />
     </>
   );
 }
