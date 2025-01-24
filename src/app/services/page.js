82ea3811e85e4ec5f3f411007f509fc60fa83c9e
aitizaz <atz.softprgmr@gmail.com>
2025-01-24 16:26:21 +0500

    added changes

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,10 +1,13 @@
+// importing services page subcomponents
 import MainHeroSection from "../components/layoutcomponents/mainherosection/mainherosection";
-import testdata from "../data/services/servicesherodata.json";
 import ImportSection from "../components/pagecomponents/servicespagecomps/importancesection";
 import RoundedCardsSection from "../components/pagecomponents/servicespagecomps/roundedcardsection";
 import ServicesBenefitsSection from "../components/pagecomponents/servicespagecomps/servicesbenefitssection";
 import TrustedBySection from "../components/pagecomponents/servicespagecomps/trustedbysection";
 import ServicesReviewsSection from "../components/pagecomponents/servicespagecomps/servicesreviewssection";
+
+// importing services page data
+import testdata from "../data/services/servicesherodata.json";
 import reviewerdata from "../data/services/reviewsdata.json";
 
 export default function Services() {
