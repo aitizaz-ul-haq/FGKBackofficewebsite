9a4820daf763f5dc80132ec7e15b95328c793e1d
aitizaz-ul-haq <atz.softprgmr@gmail.com>
2025-06-26 13:00:02 +0500

    added changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,10 +27,27 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="preload" as="image" href="/images/Data-Management-1920px-hero-section-3x-min.png" type="image/png" />
-        {/* <link rel="preload" as="image" href="/images/Data-Management-1920px-hero-section-3x-min.png" type="image/webp" /> */}
+        <link
+          rel="preload"
+          as="image"
+          href="/images/Operations-Support-1920px-hero-section-3x-compressed.png"
+          type="image/png"
+        />
+        <link
+          rel="preload"
+          as="image"
+          href="/images/Data-Management-1920px-hero-section-3x-min.png"
+          type="image/png"
+        />
       </head>
       <body className={`${inter.variable} ${robotoSerif.variable}`}>
+         <img
+          src="/images/Operations-Support-1920px-hero-section-3x-compressed.png"
+          alt=""
+          width="1"
+          height="1"
+          style={{ display: "none" }}
+        />
         <img
           src="/images/Data-Management-1920px-hero-section-3x-min.png"
           alt=""
