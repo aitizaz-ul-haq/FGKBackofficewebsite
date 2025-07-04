91e7d21041ec3c4cad466cc9f7121d4abba79039
aitizaz-ul-haq <atz.softprgmr@gmail.com>
2025-07-04 20:39:27 +0500

    added changes

--- src/app/components/subpagecomponenets/subpagesgetstartedsection/getstartedmicrocomps/getstartedsectionbutton.js
+++ src/app/components/subpagecomponenets/subpagesgetstartedsection/getstartedmicrocomps/getstartedsectionbutton.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function GetStartedSectionButton() {
   return (
-    <button className="subpagesgetstartedsection-button">
-      Hire A Professional
-    </button>
+    <Link
+      href="/contact"
+      className="general-linkage"
+      title="lets start talking"
+    >
+      <button className="subpagesgetstartedsection-button">
+        Hire A Professional
+      </button>
+    </Link>
   );
 }
