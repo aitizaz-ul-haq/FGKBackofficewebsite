6aab8ad964dbea3f7e30bb4e1828683190f2f042
aitizaz-ul-haq <atz.softprgmr@gmail.com>
2025-07-08 12:51:15 +0500

    added changes

--- src/app/components/subpagecomponenets/webdev/webdevmicrocomps/webdevbutton.js
+++ src/app/components/subpagecomponenets/webdev/webdevmicrocomps/webdevbutton.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function WebDevButton() {
   return (
-    <button className="webdev-grid-text-button font-inter">
-      Hire A Professional
-    </button>
+    <Link
+      href="/contact"
+      className="general-linkage"
+      title="Talk to our Web Professionals"
+    >
+      <button className="webdev-grid-text-button font-inter">
+        Hire A Professional
+      </button>
+    </Link>
   );
 }
