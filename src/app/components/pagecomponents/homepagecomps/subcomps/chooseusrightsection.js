359b053898d74b62b9a73234f4039657cb95101f
aitizaz-ul-haq <atz.softprgmr@gmail.com>
2025-07-10 13:12:23 +0500

    added changes

--- src/app/components/pagecomponents/homepagecomps/subcomps/chooseusrightsection.js
+++ src/app/components/pagecomponents/homepagecomps/subcomps/chooseusrightsection.js
@@ -3,13 +3,15 @@ import Image from "next/image";
 export default function ChooseUsRightSection() {
   return (
     <div className="chooseus-right-section">
-      <Image
-        src="https://i.postimg.cc/2STLT3sQ/our-motto.webp"
-        width={550}
-        height={550}
-        className="our-motto-img"
-        alt="our motto image"
-      />
+      <div className="our-motto-wrapper">
+        <Image
+          src="https://i.postimg.cc/2STLT3sQ/our-motto.webp"
+          alt="our motto image"
+          className="our-motto-img"
+          fill
+          sizes="(max-width: 768px) 90vw, 50vw"
+        />
+      </div>
     </div>
   );
 }
