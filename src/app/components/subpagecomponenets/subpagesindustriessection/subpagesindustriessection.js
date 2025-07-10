17078925a3b2d83923a65908ca6c79502d44b1db
aitizaz-ul-haq <atz.softprgmr@gmail.com>
2025-07-10 19:07:57 +0500

    added changes

--- src/app/components/subpagecomponenets/subpagesindustriessection/subpagesindustriessection.js
+++ src/app/components/subpagecomponenets/subpagesindustriessection/subpagesindustriessection.js
@@ -4,7 +4,7 @@ export default function SubPagesIndustriesSection({ heading, text }) {
   return (
     <div className="subpagesindustries-container">
       <div className="subpagesindustries-text-container">
-        <h3 className="subpagesindustries-heading font-inter">{heading}</h3>
+        <h2 className="subpagesindustries-heading font-inter">{heading}</h2>
         <p className="subpagesindustries-text font-inter">{text}</p>
       </div>
     </div>
