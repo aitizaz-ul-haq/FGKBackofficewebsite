adfd560331e0daa2c99de96fe1b4b92bda9d90c8
aitizaz-ul-haq <atz.softprgmr@gmail.com>
2025-07-14 11:35:50 +0500

    added changes

--- src/app/components/subpagecomponenets/projmanag/progmagmicrocomps/progmagbutton.js
+++ src/app/components/subpagecomponenets/projmanag/progmagmicrocomps/progmagbutton.js
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function ProgMagButton() {
   return (
-    <button className="progmag-grid-text-button font-inter">
-      <Link
-        href="/contact"
-        className="general-linkage"
-        title="Connect with our project managers"
-      >
+    <Link
+      href="/contact"
+      className="general-linkage"
+      title="Connect with our project managers"
+    >
+      <button className="progmag-grid-text-button font-inter">
         Hire A Professional
-      </Link>
-    </button>
+      </button>
+    </Link>
   );
 }
